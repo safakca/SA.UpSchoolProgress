32d31cd567bb96368cf68221b00ff9900a8cad6a
Şafak AKÇA <40564836+safakca@users.noreply.github.com>
2023-01-15 03:31:30 +0300

    [DEV] : signalRchatRoom

---
+++ LessonProjects/SignalR1/UpSchool_SignalR_Api/Hubs/MyHub.cs
@@ -0,0 +1,104 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UpSchool_SignalR_Api.Models;
+
+namespace UpSchool_SignalR_Api.Hubs;
+public class MyHub : Hub
+{
+    public static List<string> Names { get; set; } = new List<string>();
+    public int ClientCount { get; set; } = 0;
+    public static int roomCount { get; set; } = 7;
+
+    private readonly Context _context;
+
+    public MyHub(Context context)
+    {
+        _context = context;
+    }
+
+    /*
+    public async Task SendName(string name)
+    {
+        Names.Add(name);
+        await Clients.All.SendAsync("ReceiveName", name);
+    }
+    */
+    public async Task GetNames()
+    {
+        //Buda isimleri getiren bir metot olacak
+        await Clients.All.SendAsync("ReceiveNames", Names);
+    }
+    public async override Task OnConnectedAsync()
+    {
+        ClientCount++;
+        await Clients.All.SendAsync("ReceiveClientCount", ClientCount);
+
+    }
+    public async override Task OnDisconnectedAsync(Exception exception)
+    {
+        ClientCount--;
+        await Clients.All.SendAsync("ReceiveClientCount", ClientCount);
+
+    }
+    public async Task SendName(string name)
+    {
+        if (Names.Count >= roomCount)
+        {
+            await Clients.Caller.SendAsync("Error", $"Bu oda en fazla {roomCount} kişi kadar üye alabilir.");
+        }
+        else
+        {
+            Names.Add(name);
+            await Clients.All.SendAsync("ReceiveName", name);
+        }
+    }
+
+    public async Task SendNameByGroup(string name, string roomName)
+    {
+        var room = _context.Rooms.Where(x => x.RoomName == roomName).FirstOrDefault();
+        if (room != null)
+        {
+            room.Users.Add(new User
+            {
+                Name = name
+            });
+        }
+        else
+        {
+            var newRoom = new Room
+            {
+                RoomName = roomName
+            };
+            newRoom.Users.Add(new User { Name = name });
+            _context.Rooms.Add(new Room());
+
+        }
+        await _context.SaveChangesAsync();
+        await Clients.Group(roomName).SendAsync("ReceiveMessageByGroup", name, room.RoomID);
+    }
+    public async Task GetNamesByGroup()
+    {
+        var rooms = _context.Rooms.Include(x => x.Users).Select(y => new
+        {
+            roomId = y.RoomID,
+            Users = y.Users.ToList()
+        });
+        await Clients.All.SendAsync("ReceiveNamesByGroup", rooms);
+    }
+
+    public async Task AddToGroup(string roomName)
+    {
+        await Groups.AddToGroupAsync(Context.ConnectionId, roomName);
+    }
+
+    public async Task RemoveToGroup(string roomName)
+    {
+        await Groups.RemoveFromGroupAsync(Context.ConnectionId, roomName);
+    }
+}
+
+
---
+++ LessonProjects/SignalR1/UpSchool_SignalR_Client/Models/ErrorViewModel.cs
@@ -0,0 +1,7 @@
+namespace UpSchool_SignalR_Client.Models;
+public class ErrorViewModel
+{
+    public string RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
