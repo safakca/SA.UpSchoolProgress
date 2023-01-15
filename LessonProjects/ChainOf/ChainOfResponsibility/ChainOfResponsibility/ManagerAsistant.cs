9d84098e231930c9a88dd8479ff1967ef7db0e70
Şafak AKÇA <40564836+safakca@users.noreply.github.com>
2023-01-15 03:10:07 +0300

    [DEV] : chain of responsibility

---
+++ LessonProjects/ChainOf/ChainOfResponsibility/ChainOfResponsibility/ManagerAsistant.cs
@@ -0,0 +1,38 @@
+﻿using UpSchool_ChainOfResponsibility.DAL;
+using UpSchool_ChainOfResponsibility.DAL.Entities;
+
+namespace UpSchool_ChainOfResponsibility.ChainOfResponsibility;
+public class ManagerAsistant : Employee
+{
+    public override void ProcessRequest(WithdrawViewModel req)
+    {
+        if (req.Amount <= 70000)
+        {
+            using (var context = new Context())
+            {
+                BankProcess bankProcess = new BankProcess();
+                bankProcess.EmployeeName = "Şube Müdürü-Hilal Sarı";
+                bankProcess.Description = "Müşteriye talep etmiş olduğu tutarın ödemesi şube müdür yardımcısı tarafından gerçekleştirildi.";
+                bankProcess.Amount = req.Amount;
+                bankProcess.CustomerName = req.CustomerName;
+                context.BankProcesses.Add(bankProcess);
+                context.SaveChanges();
+            }
+        }
+        else if (NextApprover != null)
+        {
+            using (var context = new Context())
+            {
+                BankProcess bankProcess = new BankProcess();
+                bankProcess.EmployeeName = "Şube Müdür Yardımcısı - Hilal Sarı";
+                bankProcess.Description = "Müşterinin talep ettiği tutar yetkim dahilinde olmadığı için işlem Şube Müdürüne gönderildi.";
+                bankProcess.Amount = req.Amount;
+                bankProcess.CustomerName = req.CustomerName;
+                context.BankProcesses.Add(bankProcess);
+                context.SaveChanges();
+
+                NextApprover.ProcessRequest(req);
+            }
+        }
+    }
+}
---
+++ LessonProjects/ChainOf/ChainOfResponsibility/ChainOfResponsibility/WithdrawViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace UpSchool_ChainOfResponsibility.ChainOfResponsibility;
+public class WithdrawViewModel
+{
+    public int BankProcessID { get; set; }
+    public int Amount { get; set; }
+    public string CustomerName { get; set; }
+    public string EmployeeName { get; set; }
+    public string Description { get; set; }
+}
