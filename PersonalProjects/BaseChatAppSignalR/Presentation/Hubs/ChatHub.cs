54dc091753c57ffc3bf3ec41546a824b134efd6c
Şafak AKÇA <40564836+safakca@users.noreply.github.com>
2023-01-15 20:53:25 +0300

    [DEV] : Chat App SignalR, .net7, mvc, identity

---
+++ PersonalProjects/BaseChatAppSignalR/Presentation/Hubs/ChatHub.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using Presentation.Models;
+using System.Threading.Tasks;
+
+namespace Presentation.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public async Task SendMessage(Message message) =>
+            await Clients.All.SendAsync("receiveMessage", message);
+    }
+}
