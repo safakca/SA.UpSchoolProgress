bc77f115dcdd75d9a2184f5142ddf7b72ab3500d
Şafak AKÇA <40564836+safakca@users.noreply.github.com>
2023-01-15 03:45:56 +0300

    [DEV] : signalR2

---
+++ LessonProjects/SignalR2/UpSchool_SignalR_Api2/Models/Context.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace UpSchool_SignalR_Api2.Models;
+public class Context : DbContext
+{
+    public Context(DbContextOptions<Context> options) : base(options) { }
+    public DbSet<Electric> Electrics { get; set; }
+}
+
