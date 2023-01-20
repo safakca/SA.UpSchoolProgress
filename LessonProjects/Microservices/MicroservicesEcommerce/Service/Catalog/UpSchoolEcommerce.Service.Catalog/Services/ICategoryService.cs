672c3b3f4cec37a3f8bd8cc03fc6afd3279fd665
Şafak AKÇA <40564836+safakca@users.noreply.github.com>
2023-01-20 14:49:50 +0300

    [DEV-UP] : product and category service added

---
+++ LessonProjects/Microservices/MicroservicesEcommerce/Service/Catalog/UpSchoolEcommerce.Service.Catalog/Services/ICategoryService.cs
@@ -0,0 +1,10 @@
+﻿using UpSchoolEcommerce.Service.Catalog.Dtos;
+using UpSchoolEcommerce.Shared.Dtos;
+
+namespace UpSchoolEcommerce.Service.Catalog.Services;
+public interface ICategoryService
+{
+    Task<ResponseDto<List<CategoryDto>>> GetAllAsync();
+    Task<ResponseDto<CategoryDto>> CreateAsync(CategoryDto categoryDto);
+    Task<ResponseDto<CategoryDto>> GetByIdAsync(string id);
+}
