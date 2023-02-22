9a423d7974960bf75860bf785d18c17845005407
Şafak AKÇA <40564836+safakca@users.noreply.github.com>
2023-02-22 15:44:38 +0300

    [DEV-UP] : Added IdentityServer

--- LessonProjects/Microservices/MicroservicesEcommerce/Service/Catalog/UpSchoolEcommerce.Service.Catalog/Dtos/CategoryDto.cs
+++ LessonProjects/Microservices/MicroservicesEcommerce/Service/Catalog/UpSchoolEcommerce.Service.Catalog/Dtos/CategoryDto.cs
@@ -1,6 +1,6 @@
 ﻿namespace UpSchoolEcommerce.Service.Catalog.Dtos;
 public class CategoryDto
 {
-    public string Id { get; set; }
+    public string? Id { get; set; }
     public string Name { get; set; }
 }
--- LessonProjects/Microservices/MicroservicesEcommerce/Service/Catalog/UpSchoolEcommerce.Service.Catalog/Dtos/ProductDto.cs
+++ LessonProjects/Microservices/MicroservicesEcommerce/Service/Catalog/UpSchoolEcommerce.Service.Catalog/Dtos/ProductDto.cs
@@ -3,7 +3,7 @@
 namespace UpSchoolEcommerce.Service.Catalog.Dtos;
 public class ProductDto
 {
-    public string Id { get; set; }
+    public string? Id { get; set; }
     public string Name { get; set; }
     public string Description { get; set; }
     public decimal Price { get; set; }
